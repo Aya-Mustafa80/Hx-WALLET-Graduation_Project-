19a65ac51ebfd5b973db549e0497d6b248b653bb
AyaMustafa <yoyamustafa22@gmail.com>
2021-12-31 18:09:16 +0200

    update2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './commponents/home-page/home-page.component';
 import { LoginComponent } from './commponents/login/login.component';
 import { SignUpComponent } from './commponents/sign-up/sign-up.component';
+import { DocPageComponent } from './commponents/doc-page/doc-page.component';
 const routes: Routes = [
   { path: '', redirectTo: '/Home', pathMatch: 'full' },
   { path: 'Home', component: HomePageComponent },
