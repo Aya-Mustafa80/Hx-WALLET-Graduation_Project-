368016a9e343727f691d82d4477e4eb026dec0d3
AyaMustafa <yoyamustafa22@gmail.com>
2021-12-31 17:35:38 +0200

    update1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './commponents/home-page/home-page.component';
+import { HeaderComponent } from './commponents/header/header.component';
+import { ContentComponent } from './commponents/content/content.component';
+import { AboutComponent } from './commponents/about/about.component';
+import { FooterComponent } from './commponents/footer/footer.component';
+import { MoreAboutComponent } from './commponents/more-about/more-about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    ContentComponent,
+    AboutComponent,
+    FooterComponent,
+    MoreAboutComponent
   ],
   imports: [
     BrowserModule,
