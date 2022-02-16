c8df6e80488508e3db410ab2397e4979b6a89644
AyaMustafa <yoyamustafa22@gmail.com>
2022-02-16 15:11:58 +0200

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,6 @@ import { SignUpComponent } from './commponents/sign-up/sign-up.component';
 import { DocPageComponent } from './commponents/doc-page/doc-page.component';
 import { PharmacyPageComponent } from './commponents/pharmacy-page/pharmacy-page.component';
 import { ForgetPasswordComponent } from './commponents/forget-password/forget-password.component';
-import { SignUpDocComponent } from './commponents/sign-up-doc/sign-up-doc.component';
-import { SignUpLabTestComponent } from './commponents/sign-up-lab-test/sign-up-lab-test.component';
-import { SignUpLabXRayComponent } from './commponents/sign-up-lab-xray/sign-up-lab-xray.component';
-import { SignUpPatienComponent } from './commponents/sign-up-patien/sign-up-patien.component';
-import { SignUpPharmacyComponent } from './commponents/sign-up-pharmacy/sign-up-pharmacy.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/Home', pathMatch: 'full' },
@@ -18,11 +13,6 @@ const routes: Routes = [
   { path: 'Login', component: LoginComponent },
   { path: 'Signup', component: SignUpComponent },
   { path: 'forget pass', component: ForgetPasswordComponent },
-  { path: 'SignDoc', component: SignUpDocComponent },
-  { path: 'Signlabtest', component: SignUpLabTestComponent },
-  { path: 'SignlabXray', component: SignUpLabXRayComponent },
-  { path: 'Signpatient', component: SignUpPatienComponent },
-  { path: 'Signpharmacy', component: SignUpPharmacyComponent },
 ];
 
 @NgModule({
--- src/app/commponents/sign-up/sign-up.component.ts
+++ src/app/commponents/sign-up/sign-up.component.ts
@@ -3,13 +3,53 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+  F1 = true;
+  F2 = false;
+  F3 = false;
+  F4 = false;
 
-  ngOnInit(): void {
+  Patient() {
+    this.F1 = true;
+    this.F2 = false;
+    this.F3 = false;
+    this.F4 = false;
   }
-
+  Doc() {
+    var but = document.getElementById('first');
+    but?.setAttribute('class', '');
+    this.F1 = false;
+    this.F2 = true;
+    this.F3 = false;
+    this.F4 = false;
+  }
+  Pharmacy() {
+    var but = document.getElementById('first');
+    but?.setAttribute('class', '');
+    this.F1 = false;
+    this.F2 = false;
+    this.F3 = true;
+    this.F4 = false;
+  }
+  TestLab() {
+    var but = document.getElementById('first');
+    but?.setAttribute('class', '');
+    this.F1 = false;
+    this.F2 = false;
+    this.F3 = false;
+    this.F4 = true;
+  }
+  XRays() {
+    var but = document.getElementById('first');
+    but?.setAttribute('class', '');
+    this.F1 = false;
+    this.F2 = false;
+    this.F3 = false;
+    this.F4 = true;
+  }
 }
