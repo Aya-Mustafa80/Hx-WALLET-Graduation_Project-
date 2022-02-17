393a8c2a6b3be50ae86521e8e4caa6dccfe90bed
AyaMustafa <yoyamustafa22@gmail.com>
2022-02-17 23:23:04 +0200

    update

--- src/app/commponents/patient-page/patient-page.component.ts
+++ src/app/commponents/patient-page/patient-page.component.ts
@@ -9,4 +9,32 @@ export class PatientPageComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+  Name = '';
+  Age = '';
+  Contient = 'Prescriptions';
+  flag1 = false;
+  flag2 = false;
+  /* SEARCH */
+  SearchClick1() {
+    if (this.flag1 == false) {
+      var search1 = document.getElementsByClassName('search p1')[0];
+      search1?.setAttribute('class', 'search p1 open');
+      this.flag1 = true;
+    } else {
+      var search1 = document.getElementsByClassName('search p1 open')[0];
+      search1?.setAttribute('class', 'search p1');
+      this.flag1 = false;
+    }
+  }
+  SearchClick2() {
+    if (this.flag2 == false) {
+      var search2 = document.getElementsByClassName('search p2')[0];
+      search2?.setAttribute('class', 'search p2 open');
+      this.flag2 = true;
+    } else {
+      var search2 = document.getElementsByClassName('search p2 open')[0];
+      search2?.setAttribute('class', 'search p2');
+      this.flag2 = false;
+    }
+  }
 }
