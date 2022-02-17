d3c0c2f4849770ae63d6cdc3ed8d31d45746c2a3
AyaMustafa <yoyamustafa22@gmail.com>
2022-02-17 17:36:28 +0200

    update

--- src/app/commponents/patient-page/patient-page.component.ts
+++ src/app/commponents/patient-page/patient-page.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-patient-page',
   templateUrl: './patient-page.component.html',
-  styleUrls: ['./patient-page.component.css']
+  styleUrls: ['./patient-page.component.css'],
 })
 export class PatientPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
