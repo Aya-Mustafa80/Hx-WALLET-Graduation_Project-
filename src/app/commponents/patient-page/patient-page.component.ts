fb088758983156c49330058ce5d6ab8a638af9c9
AyaMustafa <yoyamustafa22@gmail.com>
2022-02-18 01:56:29 +0200

    update

--- src/app/commponents/patient-page/patient-page.component.ts
+++ src/app/commponents/patient-page/patient-page.component.ts
@@ -14,6 +14,57 @@ export class PatientPageComponent implements OnInit {
   Contient = 'Prescriptions';
   flag1 = false;
   flag2 = false;
+  /* // */
+  Prescription_page = true;
+  Result_Pescription = true;
+  Detail_Prescription = true;
+  /* // */
+  MedicalTest_page = false;
+  Result_Tests = false;
+  Detail_Tests = false;
+  /* // */
+  MedicalXRay_page = false;
+  Result_XRay = false;
+  Detail_XRays = false;
+  /* // */
+  ChoosePrescription() {
+    this.Prescription_page = true;
+    this.Result_Pescription = true;
+    this.Detail_Prescription = true;
+    this.MedicalTest_page = false;
+    this.Result_Tests = false;
+    this.Detail_Tests = false;
+    this.MedicalXRay_page = false;
+    this.Result_XRay = false;
+    this.Detail_XRays = false;
+  }
+  ChooseTests() {
+    var Prescrip = document.getElementById('tap-1');
+    Prescrip?.setAttribute('class', '');
+    this.Prescription_page = false;
+    this.Result_Pescription = false;
+    this.Detail_Prescription = false;
+    this.MedicalTest_page = true;
+    this.Result_Tests = true;
+    this.Detail_Tests = true;
+    this.MedicalXRay_page = false;
+    this.Result_XRay = false;
+    this.Detail_XRays = false;
+  }
+  ChooseXRays() {
+    var Prescrip = document.getElementById('ap-1');
+    Prescrip?.setAttribute('class', '');
+    this.Prescription_page = false;
+    this.Result_Pescription = false;
+    this.Detail_Prescription = false;
+    this.MedicalTest_page = false;
+    this.Result_Tests = false;
+    this.Detail_Tests = false;
+    this.MedicalXRay_page = true;
+    this.Result_XRay = true;
+    this.Detail_XRays = true;
+  }
+
   /* SEARCH */
   SearchClick1() {
     if (this.flag1 == false) {
