adfdeb82139799ef2b2ee9cf6b480e4af118494a
AyaMustafa <yoyamustafa22@gmail.com>
2022-03-01 02:59:21 +0200

    Doc_Update

--- src/app/commponents/doc-page/doc-page.component.ts
+++ src/app/commponents/doc-page/doc-page.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-doc-page',
   templateUrl: './doc-page.component.html',
-  styleUrls: ['./doc-page.component.css']
+  styleUrls: ['./doc-page.component.css'],
 })
 export class DocPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  search = false; //true
+  Show = true; //false
+  select = true; //false
+  Start() {
+    this.search = false;
+    this.Show = true;
+    this.select = true;
   }
-
 }
