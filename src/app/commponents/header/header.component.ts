85a0f1d053095467488963553fdd7e14561d4b3a
AyaMustafa <yoyamustafa22@gmail.com>
2022-07-01 21:12:07 +0200

    HeaderEdit

--- src/app/commponents/header/header.component.ts
+++ src/app/commponents/header/header.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  S_Check() {
+    var p1 = document.getElementById('S');
+    p1?.setAttribute('class', 'SIGNN EditLS');
+    var p2 = document.getElementById('L');
+    p2?.setAttribute('class', 'LOGIN');
   }
-
+  L_Check() {
+    var p1 = document.getElementById('L');
+    p1?.setAttribute('class', 'LOGIN EditLS');
+    var p2 = document.getElementById('S');
+    p2?.setAttribute('class', 'SIGNN');
+  }
 }
